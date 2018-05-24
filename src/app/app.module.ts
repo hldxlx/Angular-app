fdfcfa3d79293ebe82551fed3fde474f42817039
binsom <18300163780@163.com>
2018-05-24 19:56:59 +0800

    英雄路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService} from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
+  // 声明组件
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
+  // 该应用所需外部模块的列表
   imports: [
-    BrowserModule
+    AppRoutingModule , BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  // 服务注入的地方
+  providers: [HeroService , MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
